308a10ee7ba618cc3a3e637b40e4c5f3966d974c
panos111 <136847209+panos111a@users.noreply.github.com>
2023-07-12 23:52:38 +0300

    Add files via upload

---
+++ Cypher Debussing Surce Code.bat
@@ -0,0 +1,196 @@
+��::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB5QQA2QAE+1EbsQ5+n//NavrV0OQOMtbIrJl+eKbcMS+FHrephg4HVWyoZfKFZragevZwB5hG9a+G2GOKc=
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRmD1UcneUgAbTemKGq+R+YZubr6/O6jwg==
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB5QQA2QAE+/Fb4I5/jHzPOXqkgPcdoaNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+echo off
+chcp 65001
+:login
+cls
+mode 140,40
+title Cypher Multitool ~ Coded By @panos111 and crack by @panos111
+echo.
+echo [40;31m                             ██████╗██╗   ██╗██████╗ [40;36m██╗  ██╗███████╗██████╗ [40;31m      ███╗   ███╗██╗███╗   ██╗██╗
+echo [40;31m                            ██╔════╝╚██╗ ██╔╝██╔══██╗[40;36m██║  ██║██╔════╝██╔══██╗[40;31m      ████╗ ████║██║████╗  ██║██║
+echo [40;31m                            ██║      ╚████╔╝ ██████╔╝[40;36m███████║█████╗  ██████╔╝[40;31m█████╗██╔████╔██║██║██╔██╗ ██║██║
+echo [40;31m                            ██║       ╚██╔╝  ██╔═══╝ [40;36m██╔══██║██╔══╝  ██╔══██╗[40;31m╚════╝██║╚██╔╝██║██║██║╚██╗██║██║
+echo [40;31m                             ██████╗   ██║   ██║     [40;36m██║  ██║███████╗██║  ██║[40;31m      ██║ ╚═╝ ██║██║██║ ╚████║██║
+echo [40;31m                             ╚═════╝   ╚═╝   ╚═╝     [40;36m╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝[40;31m      ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝
+echo.
+echo.                                                              [40;31m Cracked[40;36m By[40;31m Panos111[40;31m        
+set /p user= [40;33m{[40;31mx[40;33m}[40;37mEnter Username:
+echo.
+set /p pass= [40;33m{[40;31mx[40;33m}[40;37mEnter Pass:
+if %user% == root if %pass% == root goto successful
+echo.
+echo [40;31mWrong[40;37m Login, try again...
+timeout 3 >nul
+goto login
+
+:successful
+echo.
+echo [40;32mSUCCESSFUL CRACKED BY PANOS111
+timeout 2 >nul
+goto main
+
+:main
+cls
+echo.
+echo [40;31m                             ██████╗██╗   ██╗██████╗ [40;36m██╗  ██╗███████╗██████╗ [40;31m      ███╗   ███╗██╗███╗   ██╗██╗
+echo [40;31m                            ██╔════╝╚██╗ ██╔╝██╔══██╗[40;36m██║  ██║██╔════╝██╔══██╗[40;31m      ████╗ ████║██║████╗  ██║██║
+echo [40;31m                            ██║      ╚████╔╝ ██████╔╝[40;36m███████║█████╗  ██████╔╝[40;31m█████╗██╔████╔██║██║██╔██╗ ██║██║
+echo [40;31m                            ██║       ╚██╔╝  ██╔═══╝ [40;36m██╔══██║██╔══╝  ██╔══██╗[40;31m╚════╝██║╚██╔╝██║██║██║╚██╗██║██║
+echo [40;31m                             ██████╗   ██║   ██║     [40;36m██║  ██║███████╗██║  ██║[40;31m      ██║ ╚═╝ ██║██║██║ ╚████║██║
+echo [40;31m                             ╚═════╝   ╚═╝   ╚═╝     [40;36m╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝[40;31m      ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝
+echo.
+echo        					      [40;31m╔═══════════════════[40;36m════════════════════╗
+echo        					      [40;31m║ 1. Pinger           [40;36m 8. Ascii art     ║
+echo        					      [40;31m║ 2. pScan            [40;36m 9. QuasarRat     ║
+echo        					      [40;31m║ 3. IpLookup         [40;36m 10. VPN/OVH      ║
+echo        					      [40;31m║ 4. Putty            [40;36m 11. Free booters ║
+echo        					      [40;31m║ 5. Phishing         [40;36m 12. IP Logs      ║
+echo        					      [40;31m║ 6. Acc Generator    [40;36m 99. Exit         ║
+echo        					      [40;31m║ 7. Keywords         [40;36m 00. About        ║
+echo        					      [40;31m╚═══════════════════[40;36m════════════════════╝
+echo.
+set /p action= [40;31m[root[40;36m@Cypher]$:
+if %action% == 1 goto pinger
+if %action% == 2 goto pscan
+if %action% == 3 goto iplookup
+if %action% == 4 goto putty
+if %action% == 5 goto phishing
+if %action% == 6 goto generator
+if %action% == 7 goto keywords
+if %action% == 8 goto fonts
+if %action% == 9 goto rat
+if %action% == 10 goto vpn
+if %action% == 11 goto booters
+if %action% == 12 goto iplogs
+if %action% == 00 goto about
+if %action% == 99 goto exit
+echo.
+echo [41;37mInvalid[40;37m Choice...
+timeout 2 >nul
+goto main
+
+:pinger
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\pinger.bat
+goto main
+
+:pscan
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\pScan.exe
+goto main
+
+:iplookup
+set /p action= Which one? (1, 2):
+if %action% == 1 goto devil
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\iplookupv2.bat
+goto main 
+
+:devil
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\iplookup.exe
+goto main 
+
+:putty
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\putty.exe
+goto main
+
+:phishing
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\PHISHING
+goto main
+
+:generator
+@echo off & cls
+explorer "https://freealts.pw/"
+goto main
+
+:keywords
+@echo off & cls
+explorer "https://keywordtool.io/"
+goto main
+
+:fonts
+@echo off & cls
+explorer "http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20"
+goto main
+
+:rat
+@echo off & cls
+explorer "https://github.com/quasar/QuasarRAT"
+goto main
+
+:vpn
+@echo off & cls
+explorer "https://courvix.com/vpn.php"
+goto main
+
+:rapist
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\DoxRapist.exe
+goto main
+
+:booters
+@echo off & cls
+more %cd%\multi\multi\multi\PROGRAMS\booters.txt
+pause
+goto main
+
+:iplogs
+@echo off & cls
+start %cd%\multi\multi\multi\PROGRAMS\iplog.bat
+goto main
+
+:about
+@echo off & cls
+echo [40;33m{} [40;31mThis MultiTool is made by @panos111 panos7771
+echo [40;33m{} [40;31mIf u run this .exe file in VirusTotal, it will probably come out as a trojan,
+echo [40;33m{} [40;31mbut it's not, it's only because i changed the .bat file to a .exe file so kids aren't able to steal my source code
+echo [40;33m{} [40;31m--> I don't think what you said about your source code happened hahaha
+echo [40;33m{} [40;31m--> if you want me to crack other such files, DM me on Discord
+echo [40;33m{} [40;31m--> Batch File Cofing
+echo [40;33m{} [40;31m--> Source Code Github Link --> (https://github.com/panos111a)
+echo.
+echo [40;33m{[40;31mx[40;33m} [40;31mIf you have any errors, follow these steps:
+echo.
+echo [40;36m1. Put the 'multi folder' on your Desktop
+echo 2. Put the 'Cypher' .exe file on your desktop, NOT in the folder.
+echo 3. Run! :)
+timeout 20 >nul
+goto main
+
+:exit
+exit
